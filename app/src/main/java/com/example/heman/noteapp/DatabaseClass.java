36e2774e586e8bf5340579287cb34615a8b196c1
HemanMaharjan <heman.maharjan@hotmail.co.uk>
2016-06-16 16:28:10 +0100

    I've implemented these functions in this commit: Add data to database function Get data from database function Display data in Listview cursor and  Add data to database using DailogBox.

--- app/src/main/java/com/example/heman/noteapp/DatabaseClass.java
+++ app/src/main/java/com/example/heman/noteapp/DatabaseClass.java
@@ -2,34 +2,96 @@ package com.example.heman.noteapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Heman on 05/06/2016.
  */
-public class DatabaseClass extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME ="Note.db";
-    public static final String TABLE_NAME = "note_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "DATA";
 
-    public DatabaseClass(Context context) {
-        super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
-    }
+public class DatabaseClass {
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,DATA TEXT )");
+	private static final String TAG = "DatabaseClass";
+	public static final String Note_id= "_id";
+	public static final String Note_data = "note";
 
-    }
+	
+	public static final String[] ALL_KEYS = new String[] {Note_id, Note_data};
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
-        onCreate(db);
 
-    }
+	public static final String DATABASE_NAME = "myDataBase";
+	public static final String DATABASE_TABLE = "Notedb";
+	public static final int DATABASE_VERSION = 2;
+
+	private static final String DATABASE_CREATE_SQL = 
+			"CREATE TABLE " + DATABASE_TABLE 
+			+ " (" + Note_id + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ Note_data + " TEXT NOT NUll"
+			+ ");";
+	
+	private final Context context;
+	private DatabaseHelper myDBH;
+	private SQLiteDatabase db;
+
+
+	public DatabaseClass(Context ctx) {
+		this.context = ctx;
+		myDBH = new DatabaseHelper(context);
+	}
+	
+
+	public DatabaseClass open() {
+		db = myDBH.getWritableDatabase();
+		return this;
+	}
+	
+
+	public void close() {
+		myDBH.close();
+	}
+
+	public long insertRow(String task) {
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(Note_data, task);
+				
+
+		return db.insert(DATABASE_TABLE, null, initialValues);
+	}
+	
+
+
+	public Cursor getRows() {
+		String where = null;
+		Cursor c = 	db.query(true, DATABASE_TABLE, ALL_KEYS, where, null, null, null, null, null);
+		if (c != null) {
+			c.moveToFirst();
+		}
+		return c;
+	}
+
+
+
+
+	private static class DatabaseHelper extends SQLiteOpenHelper
+	{
+		DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase _db) {
+			_db.execSQL(DATABASE_CREATE_SQL);			
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase _db, int oldVersion, int newVersion) {
+			_db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+			onCreate(_db);
+		}
+	}
+
 
 }
+
--- app/src/main/java/com/example/heman/noteapp/MainActivity.java
+++ app/src/main/java/com/example/heman/noteapp/MainActivity.java
@@ -1,21 +1,93 @@
 package com.example.heman.noteapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseClass myDb;
+    EditText noteDataText;
+    Button addNoteButton;
+    EditText user_input;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDb = new DatabaseClass(this);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+        //noteDataText = (EditText)findViewById(R.id.noteData);
 
-    }
+        addNoteButton=(Button)findViewById(R.id.addNote);
+
+        openDB();
+        addNoteButton = (Button)findViewById(R.id.addNote);
+        populateList();
+
+       addNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v) {
+                    final Dialog dialog = new Dialog(MainActivity.this);
+                    dialog.setContentView(R.layout.note_input_dailog);
+                    dialog.show();
+
+
+                    Button AddNote = (Button)dialog.findViewById(R.id.Add);
+                   final EditText Notes = (EditText)dialog.findViewById(R.id.userinput);
+                    AddNote.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        myDb.insertRow(Notes.getText().toString());
+                        populateList();
+                        dialog.cancel();
+
+
+                    }
+                });
+
+                }
+
+       });
+};
+
+
+
+    private void openDB(){
+        myDb = new DatabaseClass(this); myDb.open();}
+
+
+
+
+    public void populateList(){
+                        Cursor cursor =myDb.getRows();
+                        String[] fromfieldnames =new  String[]{DatabaseClass.Note_id,DatabaseClass.Note_data};
+                        int[] toviewids = new int[]{R.id.textViewNumber,R.id.textViewData};
+                        SimpleCursorAdapter myCursorAdapter;
+                        myCursorAdapter = new SimpleCursorAdapter(getBaseContext(),R.layout.item_layout, cursor,fromfieldnames,toviewids, 0);
+                        ListView myList =(ListView)findViewById(R.id.listViewData);
+                        myList.setAdapter(myCursorAdapter);
+
+
+
+
+        /*ListView myList =(ListView)findViewById(R.id.listViewData);
+                        myList.setAdapter(myCursorAdapter);*/
+
+
+   }
+
 }
